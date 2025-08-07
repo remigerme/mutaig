6b6d616ccb7e87192467dabb114d8ab6c30154e3
remi.germe <remi.germe@gmail.com>
2025-08-07 14:14:33 -0700

    check_valid_aiger

--- src/aig.rs
+++ src/aig.rs
@@ -7,6 +7,7 @@ pub mod dfs;
 pub mod dot;
 pub mod edge;
 pub mod error;
+mod integrity;
 pub mod node;
 mod parser;
 
@@ -590,117 +591,8 @@ impl Aig {
             }
         }
 
-        self.check_integrity()
-    }
-
-    /// Tests if the current AIG nodes are in a valid AIGER format:
-    /// - constant with id $0$
-    /// - then inputs with ids $1, ..., i$
-    /// - then latches with ids $i + 1, ..., i + l + 1$
-    /// - then and gates with ids $i + l + 1, ..., i + l + a + 1$ such as for all gate z = and(a, b),
-    ///   $id(z) \gt id(a) \geq id(b)$
-    /// You can use [`Aig::minimize_ids`] to mutate the current AIG into an AIGER-compliant AIG.
-    pub fn is_valid_aiger() -> bool {
-        todo!()
-    }
-
-    /// Checking if the AIG structure is correct.
-    /// This function was written for debug purposes, as the library is supposed to maintain
-    /// integrity of the AIG at any moment.
-    pub fn check_integrity(&self) -> Result<()> {
-        // Checking that all nodes have relevant id
-        // and perform some individual integrity checks
-        for (&id, weak_node) in &self.nodes {
-            if let Some(node) = weak_node.upgrade() {
-                if node.borrow().get_id() != id {
-                    return Err(AigError::InvalidState("incoherent node id".to_string()));
-                }
-
-                self.check_node_integrity(node)?;
-            }
-        }
-
-        // Checking that all outputs are registered as nodes
-        for output in &self.outputs {
-            let output_id = output.get_node().borrow().get_id();
-            if self.get_node(output_id).is_none() {
-                return Err(AigError::InvalidState(format!(
-                    "output ({}, {}) refers to node {} which is not in the aig",
-                    output_id,
-                    output.get_complement(),
-                    output_id
-                )));
-            }
-        }
-
-        // Checks for acyclicity
-        self.get_topological_sort()?;
-
-        Ok(())
-    }
-
-    /// Check the integrity for an individual node, that is:
-    /// - check that only `False` have id 0
-    /// - check that fanins (`AigEdge`) for latch and and gate are valid too
-    ///   (ie they refer to a known node for this AIG)
-    fn check_node_integrity(&self, node: AigNodeRef) -> Result<()> {
-        match node.borrow().deref() {
-            AigNode::False => {
-                if node.borrow().get_id() != 0 {
-                    return Err(AigError::InvalidState("invalid false node".to_string()));
-                }
-            }
-            AigNode::Input(id) => {
-                if *id == 0 {
-                    return Err(AigError::IdZeroButNotFalse);
-                }
-            }
-            AigNode::Latch { id, next, .. } => {
-                if *id == 0 {
-                    return Err(AigError::IdZeroButNotFalse);
-                }
-                self.check_edge_integrity(next)?;
-            }
-            AigNode::And {
-                id,
-                fanin0,
-                fanin1,
-                fanouts,
-            } => {
-                if *id == 0 {
-                    return Err(AigError::IdZeroButNotFalse);
-                }
-                for (fanout_id, fanout_weak) in fanouts {
-                    if let Some(fanout) = fanout_weak.upgrade() {
-                        let fanout_id_real = fanout.borrow().get_id();
-                        if *fanout_id != fanout_id_real {
-                            return Err(AigError::InvalidState(format!(
-                                "incoherent fanout node id : {} in map vs {} in reality",
-                                fanout_id, fanout_id_real
-                            )));
-                        }
-                        if !self.nodes.contains_key(&fanout.borrow().get_id()) {
-                            return Err(AigError::InvalidState(format!(
-                                "fanout {} is no longer in the AIG",
-                                fanout.borrow().get_id()
-                            )));
-                        }
-                    }
-                }
-                self.check_edge_integrity(fanin0)?;
-                self.check_edge_integrity(fanin1)?;
-            }
-        }
-        Ok(())
-    }
-
-    fn check_edge_integrity(&self, fanin: &AigEdge) -> Result<()> {
-        let id = fanin.node.borrow().get_id();
-        self.get_node(id).ok_or(AigError::InvalidState(format!(
-            "edge pointing at node {} which is not in the AIG anymore",
-            id
-        )))?;
-        Ok(())
+        self.check_integrity()?;
+        self.check_valid_aiger()
     }
 }
 
